7fd91d18c741dcc5443c66c3b3f983ab63795b8a
RGaj <rgajardo@emphasys-software.com>
2016-12-13 17:54:26 -0500

    Inventory Controller

--- ionic2-qr-scanner/QRScanner-API/Controllers/InventoryController.cs
+++ ionic2-qr-scanner/QRScanner-API/Controllers/InventoryController.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using QRScanner_API.DAL;
+using QRScanner_API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QRScanner_API.Controllers
 {
-    public class InventoryController
+    public class InventoryController: Controller
     {
+        private APIDataContext _context;
+        public InventoryController (APIDataContext api)
+        {
+            _context = api;
+        }
+        public IActionResult InventoryDescription(int id)
+        {
+            int returnVal = 0;
 
+            returnVal = _context.Products.First(x => x.ID == id).Quantity;
+            
+            return Json (new { value = returnVal });
+        }
+
+        public bool AddNewProduct( Product NewItem)
+        {
+
+            return false;
+        }
     }
 }
