ce01c3c0700b22d9e1e56c7ee17f863a2e2255c6
Trung Ngo <tngo@emphasys-software.com>
2016-12-13 17:50:25 -0500

    Finished adding models

---
+++ ionic2-qr-scanner/QRScanner-API/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRScanner_API.Models
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+    }
+}
