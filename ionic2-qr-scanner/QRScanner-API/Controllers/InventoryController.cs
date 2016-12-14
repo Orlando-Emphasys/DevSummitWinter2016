011140f3c0b14baa35d0b9c18639583a041087ff
RGaj <rgajardo@emphasys-software.com>
2016-12-13 19:57:30 -0500

    First Step into the API

--- ionic2-qr-scanner/QRScanner-API/Controllers/InventoryController.cs
+++ ionic2-qr-scanner/QRScanner-API/Controllers/InventoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QRScanner_API.DAL;
 using QRScanner_API.Models;
+using QRScanner_API.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,19 +16,68 @@ namespace QRScanner_API.Controllers
         {
             _context = api;
         }
-        public IActionResult InventoryDescription(int id)
+        public IActionResult Get(int id)
         {
-            int returnVal = 0;
 
-            returnVal = _context.Products.First(x => x.ID == id).Quantity;
-            
-            return Json (new { value = returnVal });
+            ItemQR item = _context.ItemQRs.First(x => x.ID == id);
+            Product returnVal = _context.Products.First(x => x.ID == item.ProductID);
+            String returnOffice = _context.Offices.First(x => x.ID == returnVal.OfficeId).Description;
+            Owner returnOwner = _context.Owners.First(x => x.ID == item.OwnerID);
+            Location loc = _context.Locations.First(x => x.ID == returnOwner.LocationID);
+
+            return Json (new { Brand = returnVal.Brand,
+                               Type = returnVal.Type,
+                               Quantity = returnVal.Quantity,
+                               Model = returnVal.Model,
+                               Office = returnOffice,
+                               Owner = returnOwner.Name,
+                               Location = loc.Description});
         }
 
-        public bool AddNewProduct( Product NewItem)
+        public IActionResult Post([FromBody]NewItemViewModel NewItem)
         {
+            bool returnVal = false;
+            int returnID = -1;
 
+            //Increase quantity or create a new product
+            var checkInv = _context.Products.FirstOrDefault(x => x.Brand == NewItem.brand && x.Model == NewItem.model && x.Type == NewItem.type);
+            if(checkInv != null)
+            {
+                checkInv.Quantity++;
+                _context.SaveChanges();
+            }
+            else
+            {
+                checkInv = new Product() { Brand = NewItem.brand, Model = NewItem.model, Quantity = 1, Type = NewItem.type };
+                var officeloc = _context.Offices.First(x => x.City == NewItem.city);
+                checkInv.OfficeId = officeloc.ID;
+                _context.Products.Add(checkInv);
+                _context.SaveChanges();
+            }
+            //Try to insert the itemQR
+            try
+            {
+                //Trung... you set Owner.Name to int...
+                var owner = _context.Owners.First(x => x.Name.ToString().Trim().ToLower() == NewItem.ownername.Trim().ToLower());
+                var item = new ItemQR() { SerialNumber = NewItem.serialnumber, AssignedDate = DateTime.UtcNow, ProductID = checkInv.ID, OwnerID = owner.ID };
+                _context.ItemQRs.Add(item);
+                _context.SaveChanges();
+                returnVal = true;
+                returnID = item.ID;
+            }
+            catch
+            {
+                //ERROR HANDLING
+            }
+
+            return Json(new { Success = returnVal, itemID = returnID });
+        }
+
+        public bool Add(Product AddItem)
+        {
             return false;
         }
+
+
     }
 }
